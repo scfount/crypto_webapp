d18d4adb2e5bc0453c514b8833d30b1cd1aec3bc
Steven <scfount@gmail.com>
2022-02-24 11:44:05 -0800

    update api get url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:5000/flask/crypto').then(response => {
+    axios.get('https:cryptography-web-application.herokuapp.com/flask/crypto').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
