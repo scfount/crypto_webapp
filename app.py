84889fffeacb6b84d7f2d3c7ed418fd552a585da
Steven <scfount@gmail.com>
2022-02-24 11:38:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-# from flask_cors import CORS #comment this on deployment
 from api.CryptoApiHandler import CryptoApiHandler
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-# CORS(app) #comment this on deployment
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
