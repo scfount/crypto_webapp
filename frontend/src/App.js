1545abdc3640522dc3aef8d02ca97a371f7d9a57
Steven <scfount@gmail.com>
2022-04-01 16:44:15 -0700

    add hash router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,22 @@ import Nav from './components/Nav';
 import Shift from './components/shift/Shift';
 import Vigenere from './components/vigenere/Vigenere';
 import Affine from './components/affine/Affine';
+import { HashRouter } from 'react-router-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Description from './components/Description';
 
 function App() {
 
   return (
-    <Router>
+    <HashRouter>
       <Nav />
       <Routes>
-        <Route path='/' element={<Home />} />
         <Route path='/shift' element={<Shift />} />
         <Route path='/vigenere' element={<Vigenere />} />
         <Route path='/affine' element={<Affine />} />
+        <Route path='/' element={<Home />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
